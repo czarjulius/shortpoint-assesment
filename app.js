90d0daa16eb08a34639bd8ae32b4e98fdeb78e50
czarjulius <juliusczar.jc@gmail.com>
2021-11-18 19:02:31 +0100

    simple progress bar

---
+++ app.js
@@ -0,0 +1,8 @@
+function fillBar(seconds) {
+    const bars = document.querySelectorAll(".green-bar, .red-bar, .white-bar, .blue-bar, .yellow-bar");
+
+for (i = 0; i < bars.length; ++i) {
+        bars[i].style.transition = `${seconds}s linear width`;
+        bars[i].style.width = "100%";
+  }
+  }
